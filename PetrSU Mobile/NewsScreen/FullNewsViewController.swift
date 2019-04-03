6a020e2fd932ac9dd4a9a7e4afd2e285498c10bd
JoeRedskin <36441911+JoeRedskin@users.noreply.github.com>
2019-04-03 10:15:55 +0300

    Initial Commit

---
+++ PetrSU Mobile/NewsScreen/FullNewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FullNewsViewController.swift
+//  PetrSU Mobile
+//
+//  Created by User on 03/04/2019.
+//  Copyright © 2019 petrsu. All rights reserved.
+//
+
+import UIKit
+
+class FullNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
