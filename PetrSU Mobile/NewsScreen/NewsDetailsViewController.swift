c767cf14c2efd93d74e79a89386cfdc047c36f9c
JoeRedskin <36441911+JoeRedskin@users.noreply.github.com>
2019-04-03 10:22:59 +0300

    rename

--- PetrSU Mobile/NewsScreen/NewsDetailsViewController.swift
+++ PetrSU Mobile/NewsScreen/NewsDetailsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FullNewsViewController: UIViewController {
+class NewsDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
